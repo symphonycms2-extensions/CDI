8a37d263efccef1b2c0af5f841047f81943d1666
Remie Bolte <r.bolte@gmail.com>
2011-07-20 00:23:34 +0200

    Version 0.2.0: - Add rollback support in case of SQL execution errors - Add status information of the CDI log on the preferences screen - Add a "clear CDI log" button from the preferences screen

--- extensions/cdi/extension.driver.php
+++ extensions/cdi/extension.driver.php
@@ -3,7 +3,7 @@
 	require_once(EXTENSIONS . '/cdi/lib/class.cdilogquery.php');
 	
 	Class extension_cdi extends Extension {
-
+		
 		public function about() {
 			return array(
 				'name'			=> 'Continuous Database Integration',
@@ -18,6 +18,28 @@
 		}
 		
 		public function install() {
+			if($this->__createTable()) {
+				if (!file_exists(CDIROOT)) { mkdir(CDIROOT); }
+				Symphony::Configuration()->set('enabled', 'yes', 'cdi');
+				Symphony::Configuration()->set('is-slave', 'yes', 'cdi');
+				Administration::instance()->saveConfig();
+				return true;
+			} else {
+				return false;
+				}
+		}
+		
+		public function uninstall() {
+			if($this->__dropTable()) {
+				Symphony::Configuration()->remove('cdi');
+				Administration::instance()->saveConfig();
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		private function __createTable() {
 			try{
 				Symphony::Database()->query("CREATE TABLE IF NOT EXISTS `tbl_cdi_log` (
 					  `date` DATETIME NOT NULL,
@@ -25,28 +47,27 @@
 					  `author` VARCHAR(255) NOT NULL,
 					  `url` VARCHAR(255) NOT NULL,
 					  `query_hash` VARCHAR(255) NOT NULL)");
+				return true;
 			}
 			catch(Exception $e){
 				return false;
 			}
-
-			if (!file_exists(MANIFEST . '/cdi/')) { mkdir(MANIFEST . '/cdi/'); }
-			Symphony::Configuration()->set('enabled', 'yes', 'cdi');
-			Symphony::Configuration()->set('is-slave', 'yes', 'cdi');
-			Administration::instance()->saveConfig();
-			return true;
 		}
 		
-		public function uninstall() {
+		private function __dropTable() {
 			try {
 				Symphony::Database()->query("DROP TABLE IF EXISTS `tbl_cdi_log`");
+				return true;
 			}
 			catch(Exception $e){
 				return false;
 			}
-			
-			Symphony::Configuration()->remove('cdi');
-			Administration::instance()->saveConfig();
+		}
+		
+		private function __canBeMasterInstance() {
+			if(!file_exists(CDIROOT) && is_writable(MANIFEST)) { return true; }
+			if(is_writable(CDIROOT)) { return true; }
+			else { return false; }
 		}
 
 		/*-------------------------------------------------------------------------
@@ -87,33 +108,124 @@
 		}
 		
 		public function appendPreferences($context){
+
+			// Clean the database or log files if the cd_clear action was called
+			if(isset($_POST["action"]["cdi_clear"])) {
+				if(Symphony::Configuration()->get('is-slave', 'cdi') == 'yes') {
+					Symphony::Database()->query('DELETE FROM `tbl_cdi_log`');
+				} else { 
+					CdiLogQuery::cleanLogs();
+				}
+			}
+			
+			// Create the Preferences user-interface for the CDI extension
 			$group = new XMLElement('fieldset');
 			$group->setAttribute('class', 'settings');
 			$group->appendChild(new XMLElement('legend', 'Continuous Database Integration'));
+			$group->appendChild(new XMLElement('h3','Instance Mode',array('style' => 'margin-bottom: 5px;')));
 			
-			//CDI mode
 			$div = new XMLElement('div', NULL, array('class' => 'group'));
-	
+
+			//CDI mode
 			$label = Widget::Label();
 			$input = Widget::Input('settings[cdi][is-slave]', 'yes', 'checkbox');
-			if($this->_Parent->Configuration->get('is-slave', 'cdi') == 'yes') $input->setAttribute('checked', 'checked');
-			$label->setValue($input->generate() . ' This is a "Slave" instance, no structural changes will be registered.');
-			$div->appendChild($label);
-			$group->appendChild($div);
-
-			$group->appendChild(new XMLElement('p', 'The Continuous Database Integration (CDI) extension aims on enabling the automation of propagating structural changes between environments in a DTAP setup.<br />
-													 You should have a single "master" instance (usually the development environment on which the changes are captured). You can use the REST interface to automatically update the "Slave" instances.<br />
-													 The CDI extension will log which updates have already been executed and skip those during the update run. This will prevent updates from being executed twice, thus safeguarding the integrity of the database.', array('class' => 'help')));
-			
-			
-			$context['wrapper']->appendChild($group);
-		}		
-	
-		public function savePreferences($context){
-			if(!isset($_POST['settings']['cdi']['is-slave'])){
-				Symphony::Configuration()->set('is-slave', 'no', 'cdi');
+			if($this->__canBeMasterInstance()) {
+				if($this->_Parent->Configuration->get('is-slave', 'cdi') == 'yes') { $input->setAttribute('checked', 'checked'); }
+				$label->setValue($input->generate() . ' This is a "Slave" instance (no structural changes will be registered)');
 			} else {
-				Symphony::Configuration()->set('is-slave', 'yes', 'cdi');
+				$input->setAttribute('checked', 'checked');
+				$input->setAttribute('disabled', 'disabled');
+				$label->setValue($input->generate() . ' This can only be a "Slave" instance due to insufficient write permissions.');
+			}
+			$div->appendChild($label);
+			
+			$group->appendChild($div);
+			$group->appendChild(new XMLElement('p', 'The Continuous Database Integration (CDI) extension aims on enabling the automation of propagating structural changes between environments in a DTAP setup.
+													 It is imperitive that you have a single "Master" instance (usually the development environment from which the changes are captured). This is important because the autonumbers need to be exactly the same on each database. 
+													 Be carefull about the database integrity and only switch instance mode after you have ensured that you have restored all databases from the same source.', array('class' => 'help')));
+			
+			// Clear CDI logs
+			$div = new XMLElement('div', NULL);//, array('class' => 'group'));
+			
+			$entries = new XMLElement('div',NULL);
+
+			$entryCount = 0;
+			if(Symphony::Configuration()->get('is-slave', 'cdi') == 'yes') {
+				$entries->appendChild(new XMLElement('h3','The last 5 queries executed',array('style' => 'margin-bottom: 5px;')));
+				$table = new XMLElement('table', NULL, array('cellpadding' => '0', 'cellspacing' => '0', 'border' => '0'));
+				$cdiLogEntries = Symphony::Database()->fetch("SELECT * FROM tbl_cdi_log ORDER BY `date` DESC LIMIT 0,5");
+				if(count($cdiLogEntries) > 0) {
+					foreach($cdiLogEntries as $entry) {
+						$tr = new XMLElement('tr',null);
+						$tr->appendChild(new XMLElement('td',$entry['date'],array('width' => '150')));
+						$tr->appendChild(new XMLElement('td',$entry['author']),array('width' => '200'));
+						$tr->appendChild(new XMLElement('td',$entry['query_hash']));
+						$table->appendChild($tr);
+						$entryCount++;
+					}
+				} else {
+					$tr = new XMLElement('tr',null);
+					$tr->appendChild(new XMLElement('td','No CDI queries have been executed'));
+					$table->appendChild($tr);
+				}
+				$entries->appendChild($table);
+			} else {
+				$entries->appendChild(new XMLElement('h3','The last 5 queries logged',array('style' => 'margin-bottom: 5px;')));
+				$table = new XMLElement('table', NULL, array('cellpadding' => '0', 'cellspacing' => '0', 'border' => '0'));
+				$cdiLogEntries = CdiLogQuery::getCdiLogFiles();
+				if(count($cdiLogEntries) > 0) {
+					rsort($cdiLogEntries);
+					foreach($cdiLogEntries as $entry) {
+						if($entryCount == 5) { break; }
+						$tr = new XMLElement('tr',null);
+						$tr->appendChild(new XMLElement('td',$entry . '.sql'));
+						$table->appendChild($tr);
+						$entryCount++;
+					}
+				} else {
+					$tr = new XMLElement('tr',null);
+					$tr->appendChild(new XMLElement('td','There are no entries in the CDI log'));
+					$table->appendChild($tr);
+				}
+				$entries->appendChild($table);
+			}
+			$div->appendChild($entries);
+			$group->appendChild($div);
+			
+			// CLEAR Button
+			if($entryCount != 0) {
+				$div = new XMLElement('div',NULL,array('style' => 'margin-top: 10px'));//, array('style' => 'text-align:center;'));
+				$div->appendChild(new XMLElement('button', 'Clear', array('name' => 'action[cdi_clear]', 'type' => 'submit')));
+				$div->appendChild(new XMLElement('span','&nbsp;Press "Clear" to remove all CDI log entries from disk and/or Symphony Database'));
+				$div->appendChild(new XMLElement('br',NULL));
+				$div->appendChild(new XMLElement('br',NULL));
+				$group->appendChild($div);
+				$group->appendChild(new XMLElement('p', 'You can use the "Clear" button to clean up old CDI logs. 
+														 Ensure that all your Symphony have been updated either by CDI (check the last executed queries list above) or by manually restoring the same database backup on all instances.
+														 Make sure that you clear the log files on every instance (including the "Master" instance). It is important that the database schemas are synchronized before starting with a clean sheet.', array('class' => 'help')));
+			}
+			
+			// Append preferences
+			$context['wrapper']->appendChild($group);
+		}
+
+		public function savePreferences($context){
+			if(isset($_POST['settings']['cdi']['is-slave'])){
+				if($this->__createTable()) {
+					CdiLogQuery::cleanLogs();
+					Symphony::Configuration()->set('is-slave', 'yes', 'cdi');
+				} else {
+					throw new DatabaseException("Could not create CDI database table");
+				}
+			} else if($this->__canBeMasterInstance()) {
+				if($this->__dropTable()) {
+					if(!file_exists(CDIROOT)) { mkdir(CDIROOT); }
+					Symphony::Configuration()->set('is-slave', 'no', 'cdi');
+				} else {
+					throw new DatabaseException("Could not remove CDI database table");
+				}
+			} else {
+				throw new Exception("This Symphony installation is not permitted to act as MASTER instance for the CDI extension");
 			}
 			Administration::instance()->saveConfig();
 		}
--- extensions/cdi/lib/class.cdilogquery.php
+++ extensions/cdi/lib/class.cdilogquery.php
@@ -1,139 +1,239 @@
 <?php
 
-class CdiLogQuery {
+	define("CDIROOT",MANIFEST . '/cdi',false);
 	
-	private static $isUpdating;
-	private static $lastEntryTS;
-	private static $lastEntryOrder;
+	class CdiLogQuery {
+		
+		private static $isUpdating;
+		private static $lastEntryTS;
+		private static $lastEntryOrder;
 	
-	static function getAuthor() {
-		$author = Administration::instance()->Author;
-		if (isset($author)) { 
-			return $author->getFullName(); 
-		} else { 
-			return ""; 
-		}
-	}
-	
-	static function getURL() {
-		$url = Administration::instance()->getCurrentPageURL();
-		if (is_null($url)) { $url = ""; }
-		return $url;
-	}
-	
-	static function log($query) {
-		// prevent execution on the frontend and check configuration
-		if(!class_exists('Administration')) return;
-		if(Symphony::Configuration()->get('enabled', 'cdi') == 'no') return;
-		if(Symphony::Configuration()->get('is-slave', 'cdi') == 'yes') return;
-		
-		$tbl_prefix = Symphony::Configuration()->get('tbl_prefix', 'database');
-
-		/* FILTERS */
-		// do not register changes to tbl_cdi_log
-		if (preg_match("/{$tbl_prefix}cdi_log/i", $query)) return;
-		// only structural changes, no SELECT statements
-		if (!preg_match('/^(insert|update|delete|create|drop|alter|rename)/i', $query)) return;
-		// un-tracked tables (sessions, cache, authors)
-		if (preg_match("/{$tbl_prefix}(authors|cache|forgotpass|sessions)/i", $query)) return;
-		// content updates in tbl_entries (includes tbl_entries_fields_*)
-		if (preg_match('/^(insert|delete|update)/i', $query) && preg_match("/({$config->tbl_prefix}entries)/i", $query)) return;
-		
-		$query = trim($query);
-		
-		// append query delimeter if it doesn't exist
-		if (!preg_match('/;$/', $query)) $query .= ";";
-		CdiLogQuery::persistQuery($query);
-	}
-
-	//TODO: it would be nice if it would agregate all queries executed on the same timestamp into a single file.
-	//That would also eliminate the need for an "order" field.
-	static function persistQuery($query) {
-		$ts = time();
-
-		if(CdiLogQuery::$lastEntryTS != $ts) {
-			CdiLogQuery::$lastEntryTS = $ts;
-			CdiLogQuery::$lastEntryOrder = 0;
-		} else {
-			CdiLogQuery::$lastEntryOrder++;
-		}
-
-		$hash = md5($query . $ts . CdiLogQuery::$lastEntryOrder);
-		$date = date('Y-m-d H:i:s', $ts);
-		
-		try{
-			Symphony::Database()->query("INSERT INTO `tbl_cdi_log` (`query_hash`,`author`,`url`,`date`,`order`)
-										 VALUES ('" . $hash . "','" . CdiLogQuery::getAuthor() . "','" . CdiLogQuery::getURL() . "','" . $date . "'," . CdiLogQuery::$lastEntryOrder . ")");
-			$logfile = MANIFEST . '/cdi/' . $ts . '-' . CdiLogQuery::$lastEntryOrder . '-' . $hash . '.sql';
-			file_put_contents($logfile,$query);
-			return true;
-		} catch(Exception $e) {
-			//TODO: think of some smart way of dealing with errors
-			return false;
-		}
-	}
-
-	static function rollback($query) {
-		//TODO: revert the datbase and file persistance
-	}
-	
-	static function executeQueries() {
-		//TODO: Performance Measurement to see if it is faster to fetch all rows or to iterate over available scripts 
-		//and query database to see if they have been executed.
-
-		// We should not be processing any queries when the extension is disabled or when we are the Master instance
-		// Check also exists on content page, but just to be sure!
-		if((!class_exists('Administration')) ||
-		   (Symphony::Configuration()->get('enabled', 'cdi') == 'no') ||
-		   (Symphony::Configuration()->get('is-slave', 'cdi') == 'no')) {
-			echo "WARNING: CDI is disabled or you are running the queryies on the Master instance. No queries have been executed.";
-			return;
-		}
-				
-		CdiLogQuery::$isUpdating = true;
-		
-		try {
-			$files = array();
-			if($handle = opendir(MANIFEST . '/cdi/')) {
-			    while (false !== ($file = readdir($handle))) {
-			    	if($file != '.' && $file != '..') {
-			    		if(strpos($file,".sql") !== false)
-				        	$files[] = str_replace('.sql', '', $file);
-			    	}
-			    }
-			    closedir($handle);
+		/**
+		 * Return the current author.
+		 * Only available if the author is logged into Symphony backend.
+		 */
+		private static function getAuthor() {
+			$author = Administration::instance()->Author;
+			if (isset($author)) { 
+				return $author->getFullName(); 
+			} else { 
+				return ""; 
 			}
-			sort($files);
+		}
+		
+		/**
+		 * Return the current URL from which the query is executed.
+		 * Only available if the query is executed from the Symphony backend.
+		 */
+		private static function getURL() {
+			$url = Administration::instance()->getCurrentPageURL();
+			if (is_null($url)) { $url = ""; }
+			return $url;
+		}
+		
+		/**
+		 * The CdiQueryLog::log() function is called from the Database implementation responsible for executing Symphony SQL queries
+		 * If in MASTER mode, CDI will save the query to disk allowing it to be committed to the VCS. From there it will be available
+		 * for automatic query exection by CDI slave instances (see also CdiLogQuery::executeQueries()).
+		 * @param String $query
+		 */
+		public static function log($query) {
+			// Prevent execution on the frontend and check configuration conditions
+			// Do not log the query when CDI is disabled, in SLAVE mode or busy executing queries.
+			if((!class_exists('Administration')) ||
+			   (Symphony::Configuration()->get('enabled', 'cdi') == 'no') ||
+			   (Symphony::Configuration()->get('is-slave', 'cdi') == 'yes') ||
+			   (CdiLogQuery::$isUpdating)) { return true; }
 			
-			$skipped = 0;
-			$executed = 0;
-			
-			foreach($files as $file) {
-				$parts = split('-',$file);
-				$hash = $parts[2];
-				$date = date('Y-m-d H:i:s', $parts[0]);
-				$order = $parts[1];
-				
-				$cdiLogEntry = Symphony::Database()->fetchRow(0,"SELECT * FROM tbl_cdi_log WHERE `query_hash` LIKE '" . $hash . "'");
-				if(empty($cdiLogEntry)) {
-					$query = file_get_contents(MANIFEST . '/cdi/' . $file . '.sql');
-					Symphony::Database()->query($query);
-					Symphony::Database()->query("INSERT INTO `tbl_cdi_log` (`query_hash`,`author`,`url`,`date`,`order`)
-												 VALUES ('" . $parts[2] . "','" . CdiLogQuery::getAuthor() . "','" . CdiLogQuery::getURL() . "','" . $date . "'," . $order . ")");
-					$executed++;
+			$query = trim($query);
+			$tbl_prefix = Symphony::Configuration()->get('tbl_prefix', 'database');
+	
+			/* FILTERS */
+			// do not register changes to tbl_cdi_log
+			if (preg_match("/{$tbl_prefix}cdi_log/i", $query)) return true;
+			// only structural changes, no SELECT statements
+			if (!preg_match('/^(insert|update|delete|create|drop|alter|rename)/i', $query)) return true;
+			// un-tracked tables (sessions, cache, authors)
+			if (preg_match("/{$tbl_prefix}(authors|cache|forgotpass|sessions)/i", $query)) return true;
+			// content updates in tbl_entries (includes tbl_entries_fields_*)
+			if (preg_match('/^(insert|delete|update)/i', $query) && preg_match("/({$config->tbl_prefix}entries)/i", $query)) return true;
+			// append query delimeter if it doesn't exist
+			if (!preg_match('/;$/', $query)) $query .= ";";
+	
+			// We've come far enough... let's try to save it to disk!
+			return CdiLogQuery::persistQuery($query);
+		}
+	
+		/**
+		 * If it is proven to be a valid SQL Statement worthy of logging, the persistQuery() function will
+		 * write the statement to file and log it
+		 * @param string $query The SQL Statement that will be saved to file and CDI log
+		 */
+		private static function persistQuery($query) {
+			try {
+				$ts = time();
+		
+				if(CdiLogQuery::$lastEntryTS != $ts) {
+					CdiLogQuery::$lastEntryTS = $ts;
+					CdiLogQuery::$lastEntryOrder = 0;
 				} else {
-					$skipped++;
+					CdiLogQuery::$lastEntryOrder++;
 				}
+		
+				$hash = md5($query . $ts . CdiLogQuery::$lastEntryOrder);
+				$date = date('Y-m-d H:i:s', $ts);
+				
+				try{
+					//We are only logging this to file because we do not execute CDI queries on the MASTER instance
+					//The database table `tbl_cdi_log` is removed from the database on the MASTER instance.
+					//It is the repsonsibility of the user to ensure that they only have a single MASTER instance 
+					//and that they protect the integrity of the Symphony database
+					$logfile = MANIFEST . '/cdi/' . $ts . '-' . CdiLogQuery::$lastEntryOrder . '-' . $hash . '.sql';
+					file_put_contents($logfile,$query);
+					return true;
+				} catch(Exception $e) {
+					//TODO: think of some smart way of dealing with errors, perhaps through the preference screen or a CDI Status content page?
+					CdiLogQuery::rollback($hash,$ts,$order);
+					return false;
+				}
+			} catch(Exception $e) {
+				//TODO: think of some smart way of dealing with errors, perhaps through the preference screen or a CDI Status content page?
+				return false;
 			}
-			
-			echo "OK: " . $executed . " queries executed, " . $skipped . " skipped.";
-		} catch (Exception $e) {
-			//TODO: something with error handling;
-			echo "ERROR: " . $e->getMessage();
 		}
-
-		CdiLogQuery::$isUpdating = false;
-	}
 	
+		/**
+		 * The rollback() function removes the query either from file (MASTER) or from the CDI log (SLAVE)
+		 * @param String $hash The MD5 hash created using the SQL statement, the timestamp and the execution order
+		 * @param Timestamp $timestamp The UNIX timestamp on which the query was originally logged
+		 * @param Integer $order The execution order of the query (in case of multiple executions with the same timestamp)
+		 */
+		private static function rollback($hash,$timestamp,$order) {
+			try {
+				if(Symphony::Configuration()->get('is-slave', 'cdi') == 'yes') {
+					// On the SLAVE instance we need to remove the execution log entry from the database
+					Symphony::Database()->query("DELETE FROM `tbl_cdi_log` WHERE `query_hash` LIKE '" . $hash . "'");
+				} else if(Symphony::Configuration()->get('is-slave', 'cdi') == 'no') {
+					// On the MASTER instance we need to remove the persisted SQL Statement from disk (if it exists)
+					$logfile = MANIFEST . '/cdi/' . $ts . '-' . CdiLogQuery::$lastEntryOrder . '-' . $hash . '.sql';
+					if(file_exists($logfile)) {
+						unlink($logfile);
+					}
+				} else {
+					throw new Exception("Invalid value for the CDI is-slave configuration option, 'yes' or 'no' expected.");
+				}
+			} catch(Exception $e) {
+				//TODO: think of some smart way of dealing with errors, perhaps through the preference screen or a CDI Status content page?
+				//In this case it is perhaps better to simply throw the exception because the rollback failed.
+				throw $e;
+			}
+		}
+		
+		/**
+		 * The executeQueries() function will try to execute all SQL statements that are available in the manifest folder.
+		 * It will check the CDI log to see if the statement has already been executed based on the MD5 hash.
+		 * This function can only be called by SLAVE instances
+		 */
+		public static function executeQueries() {
+			// We should not be processing any queries when the extension is disabled or when we are the Master instance
+			// Check also exists on content page, but just to be sure!
+			if((!class_exists('Administration')) ||
+			   (Symphony::Configuration()->get('enabled', 'cdi') == 'no') ||
+			   (Symphony::Configuration()->get('is-slave', 'cdi') == 'no')) {
+				echo "WARNING: CDI is disabled or you are running the queryies on the Master instance. No queries have been executed.";
+				return;
+			}
+	
+			// Prevent the CdiLogQuery::log() from persisting queries that are executed by CDI itself
+			// Technically this should not be possible because it will not log queries on a SLAVE instance
+			// and you can only run the executeQueries when in SLAVE mode. This is just to be sure.
+			CdiLogQuery::$isUpdating = true;
+			
+			try {
+				$skipped = 0;
+				$executed = 0;
+	
+				$files = CdiLogQuery::getCdiLogFiles();
+				foreach($files as $file) {
+					$parts = split('-',$file);
+					$ts = $parts[0];
+					$order = $parts[1];
+					$hash = $parts[2];
+					$date = date('Y-m-d H:i:s', $ts);
+	
+					try {
+						// Look for available CDI log entries based on the provided MD5 hash
+						$cdiLogEntry = Symphony::Database()->fetchRow(0,"SELECT * FROM tbl_cdi_log WHERE `query_hash` LIKE '" . $hash . "'");
+						
+						if(empty($cdiLogEntry)) {
+							// The query has not been found in the log, thus it has not been executed
+							// So let's execute the query and add it to the log!
+							$query = file_get_contents(MANIFEST . '/cdi/' . $file . '.sql');
+							Symphony::Database()->query("INSERT INTO `tbl_cdi_log` (`query_hash`,`author`,`url`,`date`,`order`)
+														 VALUES ('" . $parts[2] . "','" . CdiLogQuery::getAuthor() . "','" . CdiLogQuery::getURL() . "','" . $date . "'," . $order . ")");
+							Symphony::Database()->query($query);
+							$executed++;
+						} else {
+							// The query has already been executed, let's do nothing;
+							$skipped++;
+						}
+					} catch (Exception $e) {
+						//TODO: think of some smart way of dealing with errors, perhaps through the preference screen or a CDI Status content page?
+						//Due to the error we need to perform a rollback to allow this query to be executed at a later stage.
+						CdiLogQuery::rollback($hash,$ts,$order);
+						echo "ERROR: " . $e->getMessage() , ". Rollback has been executed.";
+					}
+				}
+				
+				echo "OK: " . $executed . " queries executed, " . $skipped . " skipped.";
+			} catch (Exception $e) {
+				//TODO: think of some smart way of dealing with errors, perhaps through the preference screen or a CDI Status content page?
+				echo "ERROR: " . $e->getMessage();
+			}
+	
+			// Enable CdiLogQuery::log() from persisting queries
+			CdiLogQuery::$isUpdating = false;
+		}
+	
+		/**
+		 * Returns the SQL statements files from the Manifest folder
+		 */
+		public static function getCdiLogFiles() {
+			$files = array();
+			if(file_exists(MANIFEST . '/cdi/')) {
+				if($handle = opendir(MANIFEST . '/cdi/')) {
+				    while (false !== ($file = readdir($handle))) {
+				    	if($file != '.' && $file != '..') {
+				    		if(strpos($file,".sql") !== false) {
+					        	$files[] = str_replace('.sql', '', $file);
+				    		}
+				    	}
+				    }
+				    closedir($handle);
+				}
+				sort($files);
+			}
+			return $files;
+		}
+		
+		/**
+		 * Removes all the stored log files from disk
+		 * This function can only be called by MASTER instances
+		 */
+		public static function cleanLogs() {
+			// We should not be removing any files from SLAVE instances
+			if((!class_exists('Administration')) || !defined('CDIROOT') ||
+			   (Symphony::Configuration()->get('enabled', 'cdi') == 'no') ||
+			   (Symphony::Configuration()->get('is-slave', 'cdi') == 'yes')) {
+			   	throw new Exception("Can not remove CDI log files from disk, this action is only available for MASTER instances and from within the Symphony Backend");
+			}
+	
+			if(file_exists(CDIROOT)) {
+				$files = CdiLogQuery::getCdiLogFiles();
+				foreach($files as $file) { unlink(CDIROOT . '/' . $file . '.sql'); }
+				rmdir(CDIROOT);
+			}
+		}
+	}
 
-}
+?>
